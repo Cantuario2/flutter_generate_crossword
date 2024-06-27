f4e01e218c05203a1bf5a94e7889a5c5b88f114c
cantuario2 <cantuario2@hotmail.com>
2024-06-27 00:18:09 -0300

    20240627_PuzzleGeneratorGameFinished

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+//import 'widgets/crossword_generator_app.dart';
+import 'widgets/crossword_puzzle_app.dart';
 
 void main() {
   runApp(
     ProviderScope(
       child: MaterialApp(
-        title: 'Cantuario2 Crossword Builder',
+        //title: 'Cantuario2 Crossword Builder',
+        title: 'Cantuario2 Crossword Puzzle',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           useMaterial3: true,
           colorSchemeSeed: Colors.blueGrey,
           brightness: Brightness.light,
         ),
-        home: const Scaffold(
-          body: Center(
-            child: Text(
-              'Cantuario2 starter pack with Riverpod',
-              style: TextStyle(fontSize: 24),
-            ),
-          ),
-        ),
+        //home: const CrosswordGeneratorApp(),
+        home: const CrosswordPuzzleApp(),
       ),
     ),
   );
